98eadd13ca4d7712f834c0708219c9091a659160
thodorisP <teosfp@hotmail.com>
2024-05-18 12:30:19 -0400

    commit

--- database_HER/verify_database.py
+++ database_HER/verify_database.py
@@ -5,4 +5,4 @@ loc = os.getcwd() + "/"
 with open( loc + 'database_theo.js', 'r' ) as f:
     database = js.load( f )
 
-test_database( database, Vthreshold = 0.005, log = 'theo_verify.log' )
+test_database( database, Vthreshold = 0.005, log = 'theo_verify.log' ) 
